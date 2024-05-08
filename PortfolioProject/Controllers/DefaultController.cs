87169fec44c66c76ee0e0e8c47a6bd7e23a296d0
Ahmetxyz <ahmet.akagundz@gmail.com>
2024-05-09 02:14:58 +0300

    A controller has been created and the theme has been integrated.

---
+++ PortfolioProject/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
